342a616399a4d3b2cdd19637436d9cba9dd87dba
Juan Antonio <cyberjuen@gmail.com>
2015-04-24 17:15:52 +0200

    new observation class

---
+++ viola/CObservationImageWithModel.cpp
@@ -0,0 +1,27 @@
+
+#include <mrpt/config.h>   // Precompiled headers
+
+#include "CObservationImageWithModel.h"
+
+using namespace mrpt::obs;
+using namespace mrpt::utils;
+
+// This must be added to any CSerializable class implementation file.
+IMPLEMENTS_SERIALIZABLE(CObservationImageWithModel, CObservationImage,mrpt::obs)
+
+CObservationImageWithModel::CObservationImageWithModel(void *iplImage) :
+    CObservationImage(iplImage)
+{
+    ;
+}
+
+void CObservationImageWithModel::readFromStream(mrpt::utils::CStream &in, int version)
+{
+	CObservationImage::readFromStream(in, version);
+	model = cv::Mat::zeros(100, 100, CV_8UC1);
+}
+
+void CObservationImageWithModel::writeToStream(mrpt::utils::CStream &out, int *version) const
+{
+	CObservationImage::writeToStream(out, version);
+}
---
+++ viola/CObservationImageWithModel.h
@@ -0,0 +1,42 @@
+#ifndef CObservationImageWithModel_H
+#define CObservationImageWithModel_H
+
+#include <mrpt/obs/CObservationImage.h>
+#include <mrpt/utils/CSerializable.h>
+#include <mrpt/utils/CStream.h>
+#include <mrpt/utils/CImage.h>
+#include <mrpt/obs/CObservation.h>
+#include <mrpt/otherlibs/do_opencv_includes.h>
+
+namespace mrpt
+{
+namespace obs
+{
+    DEFINE_SERIALIZABLE_PRE_CUSTOM_BASE_LINKAGE( CObservationImageWithModel , CObservationImage,OBS_IMPEXP )
+
+    /** Declares a class derived from "CObservation" that encapsules an image from a camera, whose relative pose to robot is also stored.
+         The next figure illustrate the coordinates reference systems involved in this class:<br>
+         <center>
+         <img src="CObservationImage_figRefSystem.png">
+         </center>
+     *
+     * \sa CObservation, CObservationStereoImages
+     * \ingroup mrpt_obs_grp
+     */
+    class OBS_IMPEXP CObservationImageWithModel : public CObservationImage 
+    {
+        // This must be added to any CSerializable derived class:
+        DEFINE_SERIALIZABLE( CObservationImageWithModel )
+     public:
+
+        CObservationImageWithModel(void *iplImage = NULL);
+        cv::Mat model;
+
+    }; // End of class def.
+    DEFINE_SERIALIZABLE_POST_CUSTOM_BASE_LINKAGE( CObservationImageWithModel , CObservationImage,OBS_IMPEXP )
+
+
+    } // End of namespace
+} // End of namespace
+
+#endif
