1f98cd311a142263f0d38415e4f46f20299a5189
Juan-Antonio <cyberjuen@gmail.com>
2015-08-13 17:06:52 +0000

    added more parameters

--- viola/ModelParameters.h
+++ viola/ModelParameters.h
@@ -2,13 +2,14 @@
 
 double MODEL_TRANSITION_STD_XY   = 0;
 double MODEL_TRANSITION_STD_VXY  = 0;
-double MODEL_MISSING_UNCERTAINCY_MULTIPLER = 1.2;
+double MODEL_MISSING_UNCERTAINCY_MULTIPLER = 1.05;
 
 int MISSING_TIMES_THRESHOLD = 40;
 
 double NUM_PARTICLES             = 0;
 
-constexpr float LIKEHOOD_FOUND  = 0.25;
+constexpr float LIKEHOOD_FOUND  = 0.2;
+//constexpr float LIKEHOOD_FOUND  = 0.7;
 constexpr float LIKEHOOD_UPDATE = 0.9;
 
 constexpr float ELLIPSE_FITTING_ANGLE_STEP = 2;
@@ -40,6 +41,11 @@ constexpr float MODEL_AXIS_Y_METTERS = PERSON_HEAD_Y_AXIS_METTERS;
 constexpr float MODEL_SEMIAXIS_X_METTERS = MODEL_AXIS_X_METTERS * 0.5;
 constexpr float MODEL_SEMIAXIS_Y_METTERS = MODEL_AXIS_Y_METTERS * 0.5;
 
-constexpr float DEPTH_SIGMA = 1000; //mm
+
+constexpr float DEPTH_SIGMA = 1000;
+
+constexpr float HEAD_TO_CHEST_Z_MAX_DIFFERENCE_MM = 350;
 
 using DEPTH_TYPE = uint16_t;
+
+constexpr float MINIMUM_VISIBLE_CHEST_PERCENTAGE = 0.95;
